355b09267160e15d3042f830bd1479c48c4b51b6
Travis Bock <tbock@innovateteam.com>
2020-03-16 07:49:50 -0700

    updates for new deploy url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   oauth_url: 'https://epa.maps.arcgis.com/sharing/rest/oauth2/authorize',
   oauth_client_id: 'JjhAJoIVvKhEe5FA',
   oauth_response_type: 'token',
-  oauth_redirect_uri: 'https://r9.ercloud.org/sadieV2/oauthcallback',
+  oauth_redirect_uri: 'https://r9.ercloud.org/scribeexplorer/oauthcallback',
   local_client_id: 'ttDS2qFfDXJSxCjETxKHcWeFCjmiycrGPUrKRr8T',
-  local_service_endpoint: 'https://r9.ercloud.org/sadieV2/api',
+  local_service_endpoint: 'https://r9.ercloud.org/scribeexplorer/api',
   api_version_tag: 'v1',
   default_baseloadedMap: '972c131daf9d4e83b30a98630c8f8b2b',
 };
