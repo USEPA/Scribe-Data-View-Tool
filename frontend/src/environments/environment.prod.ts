17af1131f07eb9a0e4f1057be52e2801b557caf9
Travis Bock <tbock@innovateteam.com>
2020-08-13 13:54:47 -0700

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
-  oauth_url: 'https://epa.maps.arcgis.com/sharing/rest/oauth2/authorize',
-  oauth_client_id: 'JjhAJoIVvKhEe5FA',
-  oauth_response_type: 'token',
-  oauth_redirect_uri: 'https://r9.ercloud.org/scribeexplorer/oauthcallback',
-  local_client_id: 'ttDS2qFfDXJSxCjETxKHcWeFCjmiycrGPUrKRr8T',
+  oauth_url: 'https://r9.ercloud.org/api/oauth2',
   api_url: 'https://r9.ercloud.org/scribeexplorer/api',
   api_version_tag: 'v1',
   default_baseloadedMap: '972c131daf9d4e83b30a98630c8f8b2b',
