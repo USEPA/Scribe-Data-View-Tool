9377e000f758dae3e70e5fe014a98d08d4f43846
Travis Bock <tbock@innovateteam.com>
2021-03-12 12:57:10 -0800

    remove key

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(BASE_DIR)
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sbrn8pas3(km3y8m4=ekh#zt+(&$+ja+5wi*ff57zbsqi-n88v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
