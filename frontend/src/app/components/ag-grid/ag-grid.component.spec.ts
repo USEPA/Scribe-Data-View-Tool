81f2c00f7d414ae27ee8e842724fe57b404b4c05
Travis <tbock@innovateteam.com>
2023-07-14 10:04:31 -0700

    updating

--- frontend/src/app/components/ag-grid/ag-grid.component.spec.ts
+++ frontend/src/app/components/ag-grid/ag-grid.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AgGridComponent } from './ag-grid.component';
 
@@ -6,7 +6,7 @@ describe('AgGridComponent', () => {
   let component: AgGridComponent;
   let fixture: ComponentFixture<AgGridComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AgGridComponent ]
     })
--- frontend/src/app/components/visible-columns-dialog/visible-columns-dialog.component.spec.ts
+++ frontend/src/app/components/visible-columns-dialog/visible-columns-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VisibleColumnsDialogComponent } from './visible-columns-dialog.component';
 
@@ -6,7 +6,7 @@ describe('VisibleColumnsDialogComponent', () => {
   let component: VisibleColumnsDialogComponent;
   let fixture: ComponentFixture<VisibleColumnsDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VisibleColumnsDialogComponent ]
     })
